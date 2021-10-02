1ab235aad4724657e802f309f849e4781ebd200a
Carolina Adriana Hidalgo Avila <carolina.hidalgo@ucb.edu.bo>
2021-10-02 17:18:33 -0400

    Sumar  numeros separados por simbolo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 function StringCalculator(numberString) {
+  var numbers = numberString.replace(/(\r\n|\n|\r|-|;)/gm,",");
   if (numberString === ""){
     return 0
   }else if(numberString.length === 1) { 
     return (parseInt(numberString));
   }else {
-    var res = numberString.split(',');
+    var res = numbers.split(',');
     var total = 0;
       for (var i = 0; i < res.length; i++) {
         total = total + parseInt(res[i]);
--- src/App.test.js
+++ src/App.test.js
@@ -16,4 +16,10 @@ describe("Calculadora de una cadena", () => {
   it("Sumar 3 numeros separados por coma", () => {
     expect(StringCalculator("1,2,3")).toEqual(6);
   });
+  it("Sumar numeros separados por salto", () => {
+    expect(StringCalculator("1\n2,3")).toEqual(6);
+  });
+  it("Sumar numeros separados por simbolo", () => {
+    expect(StringCalculator("1-2;3")).toEqual(6);
+  });
 });
